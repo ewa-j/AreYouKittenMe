422d28fcb733da77d50b176efe92fbb572f98386
ranjot <ranjotsd@gmail.com>
2021-11-16 17:05:19 +0000

    Initialise a end activity

--- app/src/main/java/com/example/areyoukittenme/FirstActivity.java
+++ app/src/main/java/com/example/areyoukittenme/FirstActivity.java
@@ -6,18 +6,40 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowInsets;
 
-import com.example.areyoukittenme.databinding.ActivityFullscreenBinding;
+import com.example.areyoukittenme.databinding.ActivityFirstBinding;
 
 public class FirstActivity extends AppCompatActivity {
     private View mControlsView;
     private View mContentView;
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    };
+    private final Runnable mShowPart2Runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Delayed display of UI elements
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+            mControlsView.setVisibility(View.VISIBLE);
+        }
+    };
+
 
 //    private final Runnable mShowPart2Runnable = new Runnable() {
 //        @Override
@@ -31,17 +53,36 @@ public class FirstActivity extends AppCompatActivity {
 //        }
 //    };
 
+    private final View.OnTouchListener endBtnListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            switch (motionEvent.getAction()) {
 
-    private ActivityFullscreenBinding binding;
+                case MotionEvent.ACTION_UP:
+                    Intent startShapes = new Intent(FirstActivity.this, EndActivity.class);
+                    startActivity(startShapes);
+                default:
+                    break;
+            }
+            return false;
+        }
+    };
+
+
+    private ActivityFirstBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        binding = ActivityFirstBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.endBtn.setOnTouchListener(endBtnListener);
 //        binding = ActivityFullscreenBinding.inflate(getLayoutInflater());
 //        setContentView(binding.getRoot());
 //
 //        mControlsView = binding.fullscreenContentControls;
 //        mContentView = binding.fullscreenContent;
-
     }
 }
