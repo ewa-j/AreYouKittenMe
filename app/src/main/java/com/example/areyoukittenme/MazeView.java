4096b6c7e2bf49d42ebcf306e8c797bb9d398ea2
ewa <ewa.jablonskaa@gmail.com>
2021-11-17 17:33:23 +0000

    cell class

--- app/src/main/java/com/example/areyoukittenme/MazeView.java
+++ app/src/main/java/com/example/areyoukittenme/MazeView.java
@@ -3,18 +3,55 @@ package com.example.areyoukittenme;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class MazeView extends View {
-    public MazeView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+
+    private Cell[][] cells;
+    private static final int COLS = 7, ROWS = 10;
+    private static final float WALL_THICKNESS = 4;
+    private float cellSize, hMargin, vMargin;
+    private Paint wallPaint;
+
+    public MazeView(Context context, @Nullable AttributeSet attrs) { super(context, attrs);
+
+    wallPaint = new Paint();
+    wallPaint.setColor(Color.BLACK);
+    wallPaint.setStrokeWidth(WALL_THICKNESS);
+    }
+
+
+    private void createMaze() {
+        cells = new Cell[COLS][ROWS];
+
+        for(int x=0; x<COLS; x++) {
+            for(int y=0; x<ROWS; y++) {
+                cells[x][y] = new Cell(x, y);
+            }
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawColor(Color.BLUE);
+    }
+
+    private class Cell {
+        boolean
+            topWall = true,
+            leftWall = true,
+            bottomWall = true,
+            rightWall = true;
+
+        int col, row;
+
+        public Cell(int col, int row) {
+            this.col = col;
+            this.row = row;
+        }
     }
 }
