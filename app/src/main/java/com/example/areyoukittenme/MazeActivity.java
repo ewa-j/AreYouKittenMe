3fa6e601722af00cfd5aed8d91ffd690823a7eca
Anna <aejablonska66@gmail.com>
2021-11-17 16:14:27 +0000

    blue_screen

---
+++ app/src/main/java/com/example/areyoukittenme/MazeActivity.java
@@ -0,0 +1,14 @@
+package com.example.areyoukittenme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MazeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maze);
+    }
+}
---
+++ app/src/main/java/com/example/areyoukittenme/MazeView.java
@@ -0,0 +1,20 @@
+package com.example.areyoukittenme;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class MazeView extends View {
+    public MazeView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.BLUE);
+    }
+}
