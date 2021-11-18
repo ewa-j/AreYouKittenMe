148607ed2c68bafaa27efc8343098f7a5888d9b4
ewa <ewa.jablonskaa@gmail.com>
2021-11-18 00:09:08 +0000

    grid displayed

--- app/src/main/java/com/example/areyoukittenme/MazeView.java
+++ app/src/main/java/com/example/areyoukittenme/MazeView.java
@@ -6,18 +6,19 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
-
 import androidx.annotation.Nullable;
+
 
 public class MazeView extends View {
 
     private Cell[][] cells;
-    private static final int COLS = 7, ROWS = 10;
+    private static final int COLS = 20, ROWS = 10;
     private static final float WALL_THICKNESS = 4;
     private float cellSize, hMargin, vMargin;
     private Paint wallPaint;
 
-    public MazeView(Context context, @Nullable AttributeSet attrs) { super(context, attrs);
+    public MazeView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
 
     wallPaint = new Paint();
     wallPaint.setColor(Color.BLACK);
@@ -26,12 +27,15 @@ public class MazeView extends View {
     createMaze();
     }
 
-
     private void createMaze() {
         cells = new Cell[COLS][ROWS];
 
-        for(int x=0; x<COLS; x++) {
-            for(int y=0; x<ROWS; y++) {
+//        landscape view- rows became columns?
+//        for(int x=0; x<COLS; x++) {
+//            for(int y=0; y<ROWS; y++) {
+
+        for(int y=0; y<ROWS; y++) {
+            for(int x=0; x<COLS; x++) {
                 cells[x][y] = new Cell(x, y);
             }
         }
@@ -39,63 +43,66 @@ public class MazeView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-
         canvas.drawColor(Color.LTGRAY);
+
 
         int width = getWidth();
         int height = getHeight();
 
-        if(width / height > COLS / ROWS) {
+        if (width / height < COLS / ROWS) {
             cellSize = width / (COLS + 1);
         } else {
             cellSize = height / (ROWS + 1);
         }
 
-//        if (width / COLS > height / ROWS){
-//            cellSize = height / (ROWS + 1);
-//        }
-//        else {
-//            cellSize = width / (COLS + 1);
-//        }
+////        if (width / COLS > height / ROWS){
+////            cellSize = height / (ROWS + 1);
+////        }
+////        else {
+////            cellSize = width / (COLS + 1);
+////        }
 
-        hMargin = (width-COLS*cellSize)/2;
-        vMargin = (height-ROWS*cellSize)/2;
+        hMargin = (width - COLS * cellSize) / 2;
+        vMargin = (height - ROWS * cellSize) / 2;
 
         canvas.translate(hMargin, vMargin);
 
-        for(int x=0; x<COLS; x++) {
-            for(int y=0; x<ROWS; y++) {
-                if(cells[x][y].topWall)
-                    canvas.drawLine(
-                            x*cellSize,
-                            y*cellSize,
-                            (x+1)* cellSize,
-                            y* cellSize,
-                            wallPaint);
 
-                if(cells[x][y].leftWall)
+        for(int y=0; y<ROWS; y++) {
+            for(int x=0; x<COLS; x++) {
+                if(cells[x][y].topWall) {
                     canvas.drawLine(
-                            x*cellSize,
-                            y*cellSize,
-                            x* cellSize,
-                            (y+1)* cellSize,
+                            x * cellSize,
+                            y * cellSize,
+                            (x + 1) * cellSize,
+                            y * cellSize,
                             wallPaint);
+                }
 
-                if(cells[x][y].bottomWall)
+                if(cells[x][y].leftWall) {
                     canvas.drawLine(
-                            x*cellSize,
-                            (y+1)*cellSize,
-                            (x+1)* cellSize,
-                            (y+1)* cellSize,
+                            x * cellSize,
+                            y * cellSize,
+                            x * cellSize,
+                            (y + 1) * cellSize,
                             wallPaint);
-
-                if(cells[x][y].rightWall)
+                }
+                if(cells[x][y].bottomWall) {
                     canvas.drawLine(
-                            (x+1)*cellSize,
-                            y*cellSize,
-                            (x+1)* cellSize,
-                            (y+1)* cellSize,
+                            x * cellSize,
+                            (y + 1) * cellSize,
+                            (x + 1) * cellSize,
+                            (y + 1) * cellSize,
                             wallPaint);
+                }
+                if(cells[x][y].rightWall) {
+                    canvas.drawLine(
+                            (x + 1) * cellSize,
+                            y * cellSize,
+                            (x + 1) * cellSize,
+                            (y + 1) * cellSize,
+                            wallPaint);
+                }
             }
         }
     }
