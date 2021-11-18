959427d6e3a8c4a93cad86c72b1fd5672571dc24
Xia <zeyaoliu@me.com>
2021-11-18 09:23:22 +0000

    feat: add button on first activity to link maze activity, add maze instructions on first activity, add non copyright background for main and first activity, fix: string issues

--- app/src/main/java/com/example/areyoukittenme/MazeActivity.java
+++ app/src/main/java/com/example/areyoukittenme/MazeActivity.java
@@ -2,7 +2,9 @@ package com.example.areyoukittenme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MazeActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MazeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maze);
+
+        findViewById(R.id.win).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MazeActivity.this, WinActivity.class));
+            }
+        });
+
+        findViewById(R.id.lose).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MazeActivity.this, GameOverActivity.class));
+            }
+        });
     }
+
+
 }
