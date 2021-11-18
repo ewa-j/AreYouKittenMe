4d7052aede0f16bf60bce15ffb99f5663351dd13
ewa <ewa.jablonskaa@gmail.com>
2021-11-18 08:41:24 +0000

    random maze generator, removing walls commented out

--- app/src/main/java/com/example/areyoukittenme/MazeView.java
+++ app/src/main/java/com/example/areyoukittenme/MazeView.java
@@ -12,6 +12,8 @@ import android.util.AttributeSet;
 import android.view.View;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Stack;
 
 
@@ -24,6 +26,7 @@ public class MazeView extends View {
     private Paint wallPaint;
     private BitmapShader wallTexture;
     private Bitmap hedge;
+    private Random random;
 
     public MazeView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -39,13 +42,79 @@ public class MazeView extends View {
 
     wallPaint.setShader(wallTexture);
 
+    random = new Random();
 
     createMaze();
     }
 
+    private Cell getNeighbour(Cell cell) {
+        ArrayList<Cell> neighbours = new ArrayList<>();
+
+        //left neighbour
+        if(cell.col > 0) {
+            if(!cells[cell.col-1][cell.row].visited) {
+                neighbours.add(cells[cell.col-1][cell.row]);
+            }
+        }
+
+        //right neighbour
+        if(cell.col < COLS-1) {
+            if(!cells[cell.col+1][cell.row].visited) {
+                neighbours.add(cells[cell.col+1][cell.row]);
+            }
+        }
+
+        //top neighbour
+        if(cell.row > 0) {
+            if(!cells[cell.col][cell.row-1].visited) {
+                neighbours.add(cells[cell.col][cell.row-1]);
+            }
+        }
+
+        //bottom neighbour
+        if(cell.row < ROWS-1) {
+            if(!cells[cell.col][cell.row+1].visited) {
+                neighbours.add(cells[cell.col][cell.row+1]);
+            }
+        }
+
+        if(neighbours.size() > 0) {
+            int index = random.nextInt(neighbours.size());
+            return neighbours.get(index);
+        }
+        return null;
+    }
+
+    private void removeWall(Cell current, Cell next) {
+        // neighbour below
+        if(current.col == next.col & current.row == next.row+1) {
+            current.topWall = false;
+            next.bottomWall = false;
+        }
+
+        // neighbour above
+        if(current.col == next.col & current.row == next.row-1) {
+            current.bottomWall = false;
+            next.topWall = false;
+        }
+
+        // neighbour right
+        if(current.col == next.col+1 & current.row == next.row) {
+            current.leftWall = false;
+            next.rightWall = false;
+        }
+
+        // neighbour left
+        if(current.col == next.col-1 & current.row == next.row) {
+            current.rightWall = false;
+            next.leftWall = false;
+        }
+    }
+
     private void createMaze() {
-//        Stack<Cell> stack = new Stack<>();
-//        Cell current, next;
+        // random maze creation
+        Stack<Cell> stack = new Stack<>();
+        Cell current, next;
 //
         cells = new Cell[COLS][ROWS];
 
@@ -59,112 +128,126 @@ public class MazeView extends View {
             }
         }
 
+        current = cells[0][0];
+        current.visited = true;
+        do {
+            next = getNeighbour(current);
+            if(next != null) {
+                removeWall(current, next);
+                stack.push(current);
+                current = next;
+                current.visited = true;
+            } else {
+                current = stack.pop();
+            }
+        } while(!stack.empty());
+
         // removing walls
         // first row in landscape
-        cells[0][0].bottomWall = false;
-        cells[0][0].rightWall = false;
-        cells[1][0].leftWall = false;
-        cells[1][0].rightWall = false;
-        cells[2][0].bottomWall = false;
-        cells[2][0].leftWall = false;
-        cells[3][0].bottomWall = false;
-        cells[3][0].rightWall = false;
-        cells[4][0].leftWall = false;
-        cells[4][0].rightWall = false;
-        cells[5][0].leftWall = false;
-        cells[5][0].bottomWall = false;
-        cells[6][0].rightWall = false;
-        cells[6][0].bottomWall = false;
-        cells[7][0].leftWall = false;
-        cells[7][0].rightWall = false;
-        cells[8][0].leftWall = false;
-        cells[8][0].rightWall = false;
-        cells[9][0].leftWall = false;
-        cells[9][0].bottomWall = false;
-        // second row in landscape
-        cells[0][1].bottomWall = false;
-        cells[0][1].topWall = false;
-        cells[1][1].rightWall = false;
-        cells[2][1].topWall = false;
-        cells[2][1].leftWall = false;
-        cells[3][1].topWall = false;
-        cells[3][1].rightWall = false;
-        cells[4][1].leftWall = false;
-        cells[4][1].bottomWall = false;
-        cells[5][1].rightWall = false;
-        cells[5][1].topWall = false;
-        cells[6][1].leftWall = false;
-        cells[6][1].bottomWall = false;
-        cells[6][1].topWall = false;
-        cells[7][1].rightWall = false;
-        cells[7][1].bottomWall = false;
-        cells[8][1].bottomWall = false;
-        cells[8][1].rightWall = false;
-        cells[9][1].leftWall = false;
-        cells[9][1].topWall = false;
-        // 3rd row in landscape
-        cells[0][2].topWall = false;
-        cells[0][2].rightWall = false;
-        cells[1][2].leftWall = false;
-        cells[1][2].bottomWall = false;
-        cells[2][2].bottomWall = false;
-        cells[2][2].rightWall = false;
-        cells[3][2].leftWall = false;
-        cells[4][2].rightWall = false;
-        cells[4][2].topWall = false;
-        cells[4][2].bottomWall = false;
-        cells[5][2].leftWall = false;
-        cells[5][2].bottomWall = false;
-        cells[6][2].rightWall = false;
-        cells[6][2].topWall = false;
-        cells[7][2].leftWall = false;
-        cells[7][2].topWall = false;
-        cells[8][2].topWall = false;
-        cells[8][2].bottomWall = false;
-        cells[9][2].bottomWall = false;
-        // 4th row in landscape
-        cells[0][3].bottomWall = false;
-        cells[0][3].rightWall = false;
-        cells[1][3].leftWall = false;
-        cells[1][3].rightWall = false;
-        cells[1][3].topWall = false;
-        cells[2][3].leftWall = false;
-        cells[2][3].topWall = false;
-        cells[3][3].rightWall = false;
-        cells[3][3].bottomWall = false;
-        cells[4][3].topWall = false;
-        cells[4][3].leftWall = false;
-        cells[4][3].bottomWall = false;
-        cells[5][3].topWall = false;
-        cells[5][3].rightWall = false;
-        cells[6][3].leftWall = false;
-        cells[7][3].bottomWall = false;
-        cells[7][3].rightWall = false;
-        cells[8][3].leftWall = false;
-        cells[8][3].topWall = false;
-        cells[8][3].rightWall = false;
-        cells[9][3].leftWall = false;
-        cells[9][3].topWall = false;
-        // 5th row in landscape
-        cells[0][4].topWall = false;
-        cells[0][4].rightWall = false;
-        cells[1][4].leftWall = false;
-        cells[1][4].rightWall = false;
-        cells[2][4].rightWall = false;
-        cells[2][4].leftWall = false;
-        cells[3][4].topWall = false;
-        cells[3][4].leftWall = false;
-        cells[4][4].rightWall = false;
-        cells[4][4].topWall = false;
-        cells[5][4].leftWall = false;
-        cells[6][4].rightWall = false;
-        cells[7][4].leftWall = false;
-        cells[7][4].rightWall = false;
-        cells[7][4].topWall = false;
-        cells[8][4].leftWall = false;
-        cells[8][4].rightWall = false;
-        cells[9][4].leftWall = false;
-        cells[9][4].bottomWall = false;
+//        cells[0][0].bottomWall = false;
+//        cells[0][0].rightWall = false;
+//        cells[1][0].leftWall = false;
+//        cells[1][0].rightWall = false;
+//        cells[2][0].bottomWall = false;
+//        cells[2][0].leftWall = false;
+//        cells[3][0].bottomWall = false;
+//        cells[3][0].rightWall = false;
+//        cells[4][0].leftWall = false;
+//        cells[4][0].rightWall = false;
+//        cells[5][0].leftWall = false;
+//        cells[5][0].bottomWall = false;
+//        cells[6][0].rightWall = false;
+//        cells[6][0].bottomWall = false;
+//        cells[7][0].leftWall = false;
+//        cells[7][0].rightWall = false;
+//        cells[8][0].leftWall = false;
+//        cells[8][0].rightWall = false;
+//        cells[9][0].leftWall = false;
+//        cells[9][0].bottomWall = false;
+//        // second row in landscape
+//        cells[0][1].bottomWall = false;
+//        cells[0][1].topWall = false;
+//        cells[1][1].rightWall = false;
+//        cells[2][1].topWall = false;
+//        cells[2][1].leftWall = false;
+//        cells[3][1].topWall = false;
+//        cells[3][1].rightWall = false;
+//        cells[4][1].leftWall = false;
+//        cells[4][1].bottomWall = false;
+//        cells[5][1].rightWall = false;
+//        cells[5][1].topWall = false;
+//        cells[6][1].leftWall = false;
+//        cells[6][1].bottomWall = false;
+//        cells[6][1].topWall = false;
+//        cells[7][1].rightWall = false;
+//        cells[7][1].bottomWall = false;
+//        cells[8][1].bottomWall = false;
+//        cells[8][1].rightWall = false;
+//        cells[9][1].leftWall = false;
+//        cells[9][1].topWall = false;
+//        // 3rd row in landscape
+//        cells[0][2].topWall = false;
+//        cells[0][2].rightWall = false;
+//        cells[1][2].leftWall = false;
+//        cells[1][2].bottomWall = false;
+//        cells[2][2].bottomWall = false;
+//        cells[2][2].rightWall = false;
+//        cells[3][2].leftWall = false;
+//        cells[4][2].rightWall = false;
+//        cells[4][2].topWall = false;
+//        cells[4][2].bottomWall = false;
+//        cells[5][2].leftWall = false;
+//        cells[5][2].bottomWall = false;
+//        cells[6][2].rightWall = false;
+//        cells[6][2].topWall = false;
+//        cells[7][2].leftWall = false;
+//        cells[7][2].topWall = false;
+//        cells[8][2].topWall = false;
+//        cells[8][2].bottomWall = false;
+//        cells[9][2].bottomWall = false;
+//        // 4th row in landscape
+//        cells[0][3].bottomWall = false;
+//        cells[0][3].rightWall = false;
+//        cells[1][3].leftWall = false;
+//        cells[1][3].rightWall = false;
+//        cells[1][3].topWall = false;
+//        cells[2][3].leftWall = false;
+//        cells[2][3].topWall = false;
+//        cells[3][3].rightWall = false;
+//        cells[3][3].bottomWall = false;
+//        cells[4][3].topWall = false;
+//        cells[4][3].leftWall = false;
+//        cells[4][3].bottomWall = false;
+//        cells[5][3].topWall = false;
+//        cells[5][3].rightWall = false;
+//        cells[6][3].leftWall = false;
+//        cells[7][3].bottomWall = false;
+//        cells[7][3].rightWall = false;
+//        cells[8][3].leftWall = false;
+//        cells[8][3].topWall = false;
+//        cells[8][3].rightWall = false;
+//        cells[9][3].leftWall = false;
+//        cells[9][3].topWall = false;
+//        // 5th row in landscape
+//        cells[0][4].topWall = false;
+//        cells[0][4].rightWall = false;
+//        cells[1][4].leftWall = false;
+//        cells[1][4].rightWall = false;
+//        cells[2][4].rightWall = false;
+//        cells[2][4].leftWall = false;
+//        cells[3][4].topWall = false;
+//        cells[3][4].leftWall = false;
+//        cells[4][4].rightWall = false;
+//        cells[4][4].topWall = false;
+//        cells[5][4].leftWall = false;
+//        cells[6][4].rightWall = false;
+//        cells[7][4].leftWall = false;
+//        cells[7][4].rightWall = false;
+//        cells[7][4].topWall = false;
+//        cells[8][4].leftWall = false;
+//        cells[8][4].rightWall = false;
+//        cells[9][4].leftWall = false;
+//        cells[9][4].bottomWall = false;
     }
 
     @Override
@@ -239,6 +322,7 @@ public class MazeView extends View {
             leftWall = true,
             bottomWall = true,
             rightWall = true;
+        boolean visited = false;
 
         int col, row;
 
