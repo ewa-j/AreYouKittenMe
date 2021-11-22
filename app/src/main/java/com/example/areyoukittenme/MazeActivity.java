8c0b2eb85f4fce50a5218476d9f62ab84b87827e
Anna <aejablonska66@gmail.com>
2021-11-22 14:28:22 +0000

    HP added

--- app/src/main/java/com/example/areyoukittenme/MazeActivity.java
+++ app/src/main/java/com/example/areyoukittenme/MazeActivity.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MazeActivity extends AppCompatActivity {
+
+    int hp = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maze);
+
+//        int healthPoints = deriveHealthPoints();
+//        String message = "HP " + hp;
+        TextView hpTextView = (TextView) findViewById(R.id.hp);
+        hpTextView.setText("HP " + hp);
+
 
         findViewById(R.id.win).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +37,12 @@ public class MazeActivity extends AppCompatActivity {
         });
     }
 
+//    private int GetHPPercentage(int currentHealth, int maxHealth) {
+//        return currentHealth / maxHealth * 100;
+//
+//        int maxHealth = 50
+//        int currentHealth = 50
+//        health
+//    }
 
 }
