dad33a8599116dab9cf25b17e8ca5b8f96417bf2
Xia <zeyaoliu@me.com>
2021-11-22 10:57:00 +0000

    feat: automatically shows game over screen when timer runs out

--- app/src/main/java/com/example/areyoukittenme/MazeActivity.java
+++ app/src/main/java/com/example/areyoukittenme/MazeActivity.java
@@ -6,12 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MazeActivity extends AppCompatActivity {
+
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maze);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MazeActivity.this, GameOverActivity.class);
+                startActivity(intent);
+            }
+        }, 120000);
 
         findViewById(R.id.win).setOnClickListener(new View.OnClickListener() {
             @Override
