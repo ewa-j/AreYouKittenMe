953db3d0f399c4e398e70c0906e6edb8d1e76506
Xia <zeyaoliu@me.com>
2021-11-23 11:40:18 +0000

    added total score on win screen

--- app/src/main/java/com/example/areyoukittenme/WinActivity.java
+++ app/src/main/java/com/example/areyoukittenme/WinActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class WinActivity extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class WinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win);
+
+        TextView resultLable = (TextView) FindViewById(R.id.resultLable)
 
         findViewById(R.id.restart).setOnClickListener(new View.OnClickListener() {
             @Override
